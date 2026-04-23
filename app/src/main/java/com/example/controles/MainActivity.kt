9ad772fc8214f7705116440dc0491825381fd55e
nvgs5 <nvgs502@gmail.com>
2026-04-22 21:19:55 -0600

    commit

--- app/src/main/java/com/example/controles/MainActivity.kt
+++ app/src/main/java/com/example/controles/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.example.controles
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import com.example.controles.databinding.ActivityMainBinding
+
+
+class MainViewModel: ViewModel(){
+
+    var contador: Int = 0
+
+
+}
+const val CONTADOR_KEY = "contador_key"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    companion object {
+        private const val KEY_CONTADOR = "valor_contador"
+    }
+
+    var contador = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewModel.contador = savedInstanceState?.getInt(CONTADOR_KEY) ?: 0
+
+        actualizarUI()
+
+
+        binding.button2.setOnClickListener {
+
+            viewModel.contador += 1
+
+            actualizarUI()
+            Toast.makeText(this@MainActivity, "contador: ${viewModel.contador}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(CONTADOR_KEY, viewModel.contador)
+    }
+
+
+    private fun actualizarUI() {
+        binding.textView2.text = "contador: ${viewModel.contador}"
     }
 }
