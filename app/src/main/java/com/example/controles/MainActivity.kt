5c52574a1287cbdfda3bbf75fc0391792b547a23
nvgs5 <nvgs502@gmail.com>
2026-04-23 00:06:42 -0600

    newchange

--- app/src/main/java/com/example/controles/MainActivity.kt
+++ app/src/main/java/com/example/controles/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.lifecycle.ViewModel
 import com.example.controles.databinding.ActivityMainBinding
 
 
-class MainViewModel: ViewModel(){
+
+class MainViewModel : ViewModel() {
 
     var contador: Int = 0
-
-
 }
+
+
+
 const val CONTADOR_KEY = "contador_key"
 
 
@@ -24,13 +26,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     private val viewModel: MainViewModel by viewModels()
-
-    companion object {
-        private const val KEY_CONTADOR = "valor_contador"
-    }
-
-    var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,26 +42,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        viewModel.contador = savedInstanceState?.getInt(CONTADOR_KEY) ?: 0
 
         actualizarUI()
-
 
         binding.button2.setOnClickListener {
 
             viewModel.contador += 1
 
             actualizarUI()
-            Toast.makeText(this@MainActivity, "contador: ${viewModel.contador}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "contador: ${viewModel.contador}", Toast.LENGTH_SHORT).show()
         }
     }
-
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(CONTADOR_KEY, viewModel.contador)
-    }
-
 
     private fun actualizarUI() {
         binding.textView2.text = "contador: ${viewModel.contador}"
